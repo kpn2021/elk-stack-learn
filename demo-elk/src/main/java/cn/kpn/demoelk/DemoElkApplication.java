8e9fc8afe0d96b7815ac2a753c5e2d93691094bc
liuzh <wy126lzh@126.com>
2022-06-26 15:44:03 +0800

    learn elk demo

---
+++ demo-elk/src/main/java/cn/kpn/demoelk/DemoElkApplication.java
@@ -0,0 +1,13 @@
+package cn.kpn.demoelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoElkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoElkApplication.class, args);
+	}
+
+}
