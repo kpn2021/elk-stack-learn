139090d1f248144dc0231c651f0eae005c9b02a9
liuzh <wy126lzh@126.com>
2022-06-26 15:45:45 +0800

    learn-springdata-es

---
+++ demo-spring-data-es/src/main/java/cn/kpn/demospringdataes/DemoSpringDataEsApplication.java
@@ -0,0 +1,13 @@
+package cn.kpn.demospringdataes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringDataEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringDataEsApplication.class, args);
+	}
+
+}
